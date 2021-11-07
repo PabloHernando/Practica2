2cab8ce4ef4179f7bcb1f135a803c67c66255064
Pablo <pablo.hernando.izquierdo@gmail.com>
2021-11-07 18:49:09 +0100

    Second Commit

---
+++ src/app.ts
@@ -0,0 +1,80 @@
+import { Db } from "mongodb";
+import { getAndSaveRickyMortyCharacters } from "./populatedb";
+import express from "express";
+import { character, characters, register, login, signOut, deleteUser } from "./resolvers";
+
+const run = async () => {
+  const db: Db = await getAndSaveRickyMortyCharacters();
+  const app = express();
+  app.set("db", db);
+
+  app.use(`/characters`, async(req, res, next) => {
+    if(req.headers.token != undefined || req.headers.token != null){
+      const user = await db.collection("Registered_Users").findOne({token: req.headers.token})
+      if(user){
+        next();
+      }else{
+        res.json({status:409,message:"you arent logged in."})
+      }
+    }else{
+      res.json({status:409,message:"you arent logged in."})
+    }
+  });
+  app.use(`/characters/:id`, async(req, res, next) => {
+    if(req.headers.token != undefined || req.headers.token != null){
+      const user = await db.collection("Registered_Users").findOne({token: req.headers.token})
+      if(user){
+        next();
+      }else{
+        res.json({status:409,message:"you arent logged in."})
+      }
+    }else{
+      res.json({status:409,message:"you arent logged in."})
+    }
+  });
+  app.use(`/signOut`, async(req, res, next) => {
+    if(req.headers.token != undefined || req.headers.token != null){
+      const user = await db.collection("Registered_Users").findOne({token: req.headers.token})
+      if(user){
+        console.log(req.headers.token);
+        next();
+      }else{
+        res.json({status:409,message:"you arent logged in."})
+      }
+    }else{
+      res.json({status:409,message:"you arent logged in."})
+    }
+  });
+  app.use(`/deleteUser`, async(req, res, next) => {
+    if(req.headers.token != undefined || req.headers.token != null){
+      const user = await db.collection("Registered_Users").findOne({token: req.headers.token})
+      if(user){
+        console.log(req.headers.token);
+        next();
+      }else{
+        res.json({status:409,message:"you arent logged in."})
+      }
+    }else{
+      res.json({status:409,message:"you arent logged in."})
+    }
+  });
+
+  app.get("/status", async (req, res) => {
+    res.json({status:200,message:"Working."});
+  });
+
+  app.get("/characters", characters);
+  app.get("/character/:id", character);
+  app.put("/register",register);
+  app.get("/login", login);
+  app.get("/signOut", signOut);
+  app.get("/deleteUser", deleteUser);
+
+  await app.listen(3000);
+};
+
+try {
+  run();
+} catch (e) {
+  console.error(e);
+}
---
+++ src/resolvers.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from "express";
+import { Db } from "mongodb";
+import { v4 as uuidv4 } from 'uuid';
+
+export const characters = async (req: Request, res: Response) => {
+  const page = parseInt(req.query.page as string) || 1;
+  const pageSize = parseInt(req.query.pageSize as string) || 20;
+  const db: Db = req.app.get("db");
+  const chars = await db
+    .collection("Characters")
+    .find()
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
+    .toArray();
+  res.status(200).json(chars);
+};
+
+export const character = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const db: Db = req.app.get("db");
+  const char = await db.collection("Characters").findOne({ id: parseInt(id) });
+  if (char) res.status(200).json(char);
+  else res.json({status:404,message:"Character not found."})
+};
+
+export const register = async (req: Request, res: Response) => {
+  const username = req.query.username;
+  const password = req.query.password;
+  const db: Db = req.app.get("db");
+  const user = await db.collection("Registered_Users").findOne({user: username});
+  if(!user){
+    const char = await db.collection("Registered_Users").insertOne({
+      user:username,
+      password: password,
+      token: null,
+    });
+    res.json({status:"200",message:"Register succesful."})
+  }else{
+    res.json({status:"409",message:"Username already in use."})
+  }
+}
+
+export const login = async (req:Request, res: Response) => {
+  const username = req.query.username;
+  const password = req.query.password;
+  const db: Db = req.app.get("db");
+  const user = await db.collection("Registered_Users").findOne({user: username, password: password});
+  const token = uuidv4();
+  await db.collection("Registered_Users").findOneAndUpdate({user: username},{'$set' : {token:token}});
+  if(user){
+    res.json({status:"200",message:"Login succesful",token:token})
+  }else{
+    res.json({status:404,message:"Wrong user or password."})
+  }
+}
+
+export const signOut = async (req:Request, res: Response) => {
+  const token = req.headers.token;
+  const db: Db = req.app.get("db");
+  const user = await db.collection("Registered_Users").findOne({token: token});
+  if(user){
+    await db.collection("Registered_Users").findOneAndUpdate({token: token},{'$set' : {token:null}});
+    res.json({status:"200",message:"SignOut succesful"})
+  }else{
+    res.json({status:404,message:"User not found"})
+  }
+}
+
+export const deleteUser = async (req:Request, res: Response) => {
+  const token = req.headers.token;
+  const db: Db = req.app.get("db");
+  const user = await db.collection("Registered_Users").findOne({token: token});
+  if(user){
+    await db.collection("Registered_Users").findOneAndDelete({token: token});
+    res.json({status:"200",message:"User deleted"})
+  }else{
+    res.json({status:404,message:"User not found."})
+  }
+}
+
+
